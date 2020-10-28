b34d6051418b084c1825b32a8d02219420a69bb3
Weronika Ludwiczuk <weronika.ludwiczuk@gmail.com>
2020-10-28 18:43:50 +0100

    FirstClass

---
+++ src/main/java/FirstClass.java
@@ -0,0 +1,20 @@
+public class FirstClass {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!");
+        System.out.println("Weronika");
+        System.out.println("34");
+        char ch = 'L';
+        String str = String.valueOf(ch);
+        System.out.println(str);
+
+
+
+
+    }
+}
+
+
+
+
+
+
