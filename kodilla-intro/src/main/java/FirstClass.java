2f91dad8cec413b14b0b8a666b1a8cf90d159eea
Weronika Ludwiczuk <weronika.ludwiczuk@gmail.com>
2020-11-15 21:21:54 +0100

    FirstClass

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class FirstClass {
+public class FirstClass {
+    public static void main(String[] args) {
+
+        Notebook notebook = new Notebook(200, 600, 2014);
+        checkNotebook(notebook);
+        Notebook heavyNotebook = new Notebook(3000, 1390, 1980);
+        checkNotebook(heavyNotebook);
+        Notebook oldNotebook = new Notebook(2001, 400, 2013);
+        checkNotebook(oldNotebook);
+    }
+
+    public static void checkNotebook(Notebook notebook) {  //notebook jako parametr
+        System.out.println(notebook.weight + "gram " + notebook.price + " " + notebook.year);
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkYearPerWeight();
+    }
+
 }
