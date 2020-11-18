a0e4fae78cc1e519607abd1b9438ba978ad69805
Weronika Ludwiczuk <weronika.ludwiczuk@gmail.com>
2020-11-18 19:36:48 +0100

    SecondClass

--- kodilla-intro/src/main/java/SecondClass.java
+++ kodilla-intro/src/main/java/SecondClass.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class SecondClass {
+public class SecondClass {
+
+    public static void main(String[] args) {
+        Grades grades = new Grades();
+
+        int[] gradesArray = {6,2,3,1,4};
+
+
+
+        for (int i = 0; i < gradesArray.length; i++) {
+            grades.add(gradesArray[i]);
+            System.out.println(grades.lastGrade());
+            System.out.println("Srednia arytmetyczna wynosi " + grades.calculateAverage());
+
+
+        }
+
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
