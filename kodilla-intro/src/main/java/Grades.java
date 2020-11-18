6096c9d185b1fa53861aaf5ea40cea1688ddc371
Weronika Ludwiczuk <weronika.ludwiczuk@gmail.com>
2020-11-18 18:38:09 +0100

    Grades

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Grades {
+public class Grades {
+    private int[] grades;
+    private int size; //zmienna monitoruje aktualna liczbę elementów w tablicy
+
+    public Grades() {   //konstruktor
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+    public void add(int value) {  //przerywa działanie - void zwraca pustkę
+        if (this.size == 10) {
+            System.out.println("Can't add this grade!!!");
+            return;
+        }
+        this.grades[this.size] = value; //dodanie elementu do tablicy, jeśli rozmiar jest mniejszy od maksymalnego
+        this.size++;
+    }
+
+    public int lastGrade() {
+
+        return this.grades[size - 1];
+    }
+
+
+    public double calculateAverage() {
+        int sum = 0;
+
+
+        for (int i = 0; i < grades.length; i++) {
+            sum += this.grades[i];
+        }
+        return (this.size == 0) ? 0 : (double)sum/(double)this.size;
+
+    }
+
+
+
 }
+
+
+
