12b6f02063f8313dc950e1bbf102fea90321de65
Weronika Ludwiczuk <weronika.ludwiczuk@gmail.com>
2020-11-23 13:30:06 +0100

    User

---
+++ kodilla-intro/src/main/java/User.java
@@ -0,0 +1,29 @@
+import java.io.OptionalDataException;
+
+public class User {
+
+    private int age;
+    private String name;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
+
+
+
+
+
+
+
+
