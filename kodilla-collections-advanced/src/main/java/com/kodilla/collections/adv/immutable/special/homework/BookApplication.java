ec922d880f1742fdeea00c65cccd38eacdae0459
Weronika Ludwiczuk <weronika.ludwiczuk@gmail.com>
2021-01-16 19:42:22 +0100

    5.4. Księgozbiór - remove hashcode()

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -7,9 +7,8 @@ public class BookApplication {
         Book book2 = BookManager.createBook("Pride and Prejudice","Jane Austin");
         Book serotonine = BookManager.createBook("Serotonine","Michel Houllebecq");
 
-        System.out.println(book1.hashCode());
-        System.out.println(book2.hashCode());
-        System.out.println(serotonine.hashCode());
+        System.out.println("Addresses are the same: " + (book1 == book2));
+        System.out.println("Addresses are the same: " + (serotonine == book2));
 
         Book serotonineAgain = BookManager.createBook("Serotonine","Michel Houllebecq");
 
