94fcdb798f5754f440d36aba682f7785bd0ece6f
Weronika Ludwiczuk <weronika.ludwiczuk@gmail.com>
2021-01-20 19:31:09 +0100

    6.1. Zadanie: uczniowie i nauczyciele ze Streamem.

--- kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -3,27 +3,49 @@ package com.kodilla.optional.homework;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
         List<Student> register = new ArrayList<>();
-        register.add(new Student("Tom Wise",null ));
-        register.add(new Student("Mila Cunnis", new Teacher("Bill") ));
-        register.add(new Student("Kasia Cichopek", new Teacher("Grzegorz") ));
+        register.add(new Student("Tom Wise", null));
+        register.add(new Student("Mila Cunnis", new Teacher("Bill")));
+        register.add(new Student("Kasia Cichopek", new Teacher("Grzegorz")));
         register.add(new Student("Marcin Najman", null));
 
+        var studentsWithTeachers = register
+            .stream()
+            .filter(student -> Optional.ofNullable(student.getTeacher()).isPresent()) //  wyfiltruj tych co mają teachera
+            .map(student -> "Student '" + student.getName() + "'; Teacher '" + student.getTeacher().getName() + "'")
+            .collect(Collectors.toList());
 
-        for(Student student: register) {
-            Optional<Teacher> optionalTeacher = Optional.ofNullable(student.getTeacher());
+        System.out.println(studentsWithTeachers);
 
-            System.out.println("Student: " + student.getName());
-            if(optionalTeacher.isPresent()) {
-                Teacher teacher = optionalTeacher.get();
-                System.out.println("Teacher:" + teacher.getName());
-            } else {
-                System.out.println("Teacher: <undefined>");
-            }
-            System.out.println("----------------------------------");
-        }
+        var studentsWithoutTeachers = register
+            .stream()
+            .filter(student -> Optional.ofNullable(student.getTeacher()).isEmpty())
+            .map(student -> "Student '" + student.getName() + "'; Teacher <undefined>")
+            .collect(Collectors.toList());
+
+        System.out.println(studentsWithoutTeachers);
+
+
+        //for(Student student: register) {
+          //  Optional<Teacher> optionalTeacher = Optional.ofNullable(student.getTeacher());
+
+            //System.out.println("Student: " + student.getName());
+            //if(optionalTeacher.isPresent()) {
+              //  Teacher teacher = optionalTeacher.get();
+               // System.out.println("Teacher:" + teacher.getName());
+            //} else {
+              //  System.out.println("Teacher: <undefined>");
+            //}
+            //System.out.println("----------------------------------");
+        //}
     }
-}
+ }
+
+
+
+
+
